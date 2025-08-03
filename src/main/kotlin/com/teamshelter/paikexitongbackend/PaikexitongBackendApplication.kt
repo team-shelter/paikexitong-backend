9030cb6a956852223cfb6da74ccd9ea92bd6699c
AlydiaHZ <zrj55845@gmail.com>
2025-08-03 17:59:04 +0800

    feat: 初始化 spring boot kotlin

---
+++ src/main/kotlin/com/teamshelter/paikexitongbackend/PaikexitongBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.teamshelter.paikexitongbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaikexitongBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaikexitongBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamshelter/paikexitongbackend/PaikexitongBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamshelter.paikexitongbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaikexitongBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
